683658bd42e046cdf21e0b3374b1917be1395c26
unknown <pnp3737@gmail.com>
2020-05-30 20:47:53 +0900

    05302047.commit

--- keras/keras76_iris_dnn.py
+++ keras/keras76_iris_dnn.py
@@ -19,7 +19,6 @@ print(type(iris)) # <class 'sklearn.utils.Bunch'>
 # 해당 부분에 alt+F8 키로 뭐가 문제인지 보니까 Instance of 'tuple' has no 'target' member 이란 메세지가 떴다
 # 구글링 해서 찾아본 결과 , x = iris['data], y = ['target'] 이라고 바꿔주니까 빨간 줄이 사라졌다!
 
-
 print(x)
 print(y)
 
@@ -56,6 +55,72 @@ model = Sequential()
 
 model.add(Dense(33, input_shape = (4, )))
 model.add(Dense(55))
+model.add(Dropout(0.1))
+model.add(Dense(77, activation = 'relu'))
+model.add(Dense(111))
+model.add(Dropout(0.3))
+model.add(Dense(133, activation = 'relu'))
+model.add(Dropout(0.4))
+model.add(Dense(122, activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(88, activation = 'relu'))
+model.add(Dense(66))
+model.add(Dense(44, activation = 'relu'))
+model.add(Dense(33))
+model.add(Dropout(0.2))
+model.add(Dense(3, activation = 'softmax'))
+
+model.summary()
+
+# 3. 컴파일, 훈련
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+
+# es = EarlyStopping(monitor = 'loss', patience = 50, mode = 'auto')
+
+modelpath = './model/{epoch:02d}--{val_loss:.4f}.hdf5'
+
+checkpoint = ModelCheckpoint(filepath = modelpath, monitor = 'loss', save_best_only = True, mode = 'auto')
+
+# tb_hist = TensorBoard(log_dir='graph', histogram_freq=0,
+#                       write_graph=True, write_images=True)
+
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
+hist = model.fit(x_train, y_train, epochs = 50, batch_size = 1, validation_split = 0.2, callbacks = [checkpoint], verbose = 1)
+
+plt.figure(figsize = (10, 6)) 
+
+plt.subplot(2, 1, 1)
+plt.plot(hist.history['loss'], marker = '.', c = 'red', label = 'loss')         
+plt.plot(hist.history['val_loss'], marker = '.', c = 'blue', label = 'val_loss')   
+plt.grid()
+plt.title('loss')      
+plt.ylabel('loss')      
+plt.xlabel('epoch')          
+plt.legend(loc = 'upper right') 
+
+plt.subplot(2, 1, 2)
+plt.plot(hist.history['acc'], marker = '*', c = 'green', label = 'acc')
+plt.plot(hist.history['val_acc'], marker = '*', c = 'purple', label = 'val_acc')
+plt.grid() 
+plt.title('acc')      
+plt.ylabel('acc')      
+plt.xlabel('epoch')          
+plt.legend(loc = 'upper right') 
+plt.show()  
+
+# 4. 평가, 예측
+loss, acc  = model.evaluate(x_test, y_test, batch_size = 1)
+
+print('loss : ', loss)
+print('acc : ', acc)
+
+# y_pred = model.predict(x_test)
+# print(y_pred)
+# print(np.argmax(y_pred, axis = 1))
+
+'''
+model.add(Dense(33, input_shape = (4, )))
+model.add(Dense(55))
 model.add(Dense(77))
 model.add(Dense(99))
 model.add(Dense(88))
@@ -64,24 +129,50 @@ model.add(Dense(44, activation = 'relu'))
 model.add(Dense(33))
 model.add(Dense(3, activation = 'softmax'))
 
-model.summary()
-
-# 3. 컴파일, 훈련
-
-model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
-model.fit(x_train, y_train, epochs = 100, batch_size = 1, validation_split = 0.2, verbose = 1)
-
-# 4. 평가, 예측
-loss, acc  = model.evaluate(x_test, y_test, batch_size = 1)
-
-print('loss : ', loss)
-print('acc : ', acc)
-
-y_pred = model.predict(x_test)
-print(y_pred)
-print(np.argmax(y_pred, axis = 1))
-
-'''
+epo 100, val 0.2, batch 1, es x
 loss :  0.019614876400736374
+acc :  1.0
+'''
+'''
+model.add(Dense(33, input_shape = (4, )))
+model.add(Dense(55))
+model.add(Dense(77, activation = 'relu'))
+model.add(Dense(111))
+model.add(Dropout(0.3))
+model.add(Dense(133))
+model.add(Dropout(0.4))
+model.add(Dense(122))
+model.add(Dropout(0.2))
+model.add(Dense(88, activation = 'relu'))
+model.add(Dense(66))
+model.add(Dense(44, activation = 'relu'))
+model.add(Dense(33))
+model.add(Dense(3, activation = 'softmax'))
+
+epo 300, es 50, val 0.3, batch 1, best 127, 0.1848(loss)
+loss :  0.15025983566220777
+acc :  0.9333333373069763
+'''
+'''
+3차
+model.add(Dense(33, input_shape = (4, )))
+model.add(Dense(55))
+model.add(Dropout(0.1))
+model.add(Dense(77, activation = 'relu'))
+model.add(Dense(111))
+model.add(Dropout(0.3))
+model.add(Dense(133, activation = 'relu'))
+model.add(Dropout(0.4))
+model.add(Dense(122, activation = 'relu'))
+model.add(Dropout(0.2))
+model.add(Dense(88, activation = 'relu'))
+model.add(Dense(66))
+model.add(Dense(44, activation = 'relu'))
+model.add(Dense(33))
+model.add(Dropout(0.2))
+model.add(Dense(3, activation = 'softmax'))
+
+epo 150, es x, val 0.2, batch 1, best 43, 0.0223
+loss :  0.02353709747027679
 acc :  1.0
 '''
