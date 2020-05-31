8c9859f6c7acf80a9fbe2cd23df32d79cc571f55
unknown <pnp3737@gmail.com>
2020-05-31 16:18:21 +0900

    05311618.commit

--- keras/keras78_iris_cnn.py
+++ keras/keras78_iris_cnn.py
@@ -55,17 +55,14 @@ x_test = x_test.reshape(30, 2, 2, 1)
 # 2. 모델 구성
 model = Sequential()
 
-model.add(Conv2D(11, (2, 2), input_shape = (2, 2, 1)))
-model.add(Conv2D(22, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(44, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(66, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(99, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(111, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(88, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(55, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(33, (2, 2), padding = 'same', activation = 'relu'))
-model.add(Conv2D(11, (2, 2), padding = 'same', activation = 'relu'))
+model.add(Conv2D(50, (2, 2), input_shape = (2, 2, 1)))
+model.add(Conv2D(70, (2, 2), padding = 'same'))
 model.add(Flatten())
+model.add(Dense(90))
+model.add(Dense(60))
+model.add(Dense(40))
+model.add(Dense(20))
+model.add(Dense(10))
 model.add(Dense(3, activation = 'softmax'))
 
 model.summary()
@@ -73,7 +70,7 @@ model.summary()
 # # 3. 컴파일, 훈련
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 
-es = EarlyStopping(monitor = 'loss', patience = 10, mode = 'auto')
+# es = EarlyStopping(monitor = 'loss', patience = 10, mode = 'auto')
 modelpath = './model/{epoch:02d}--{acc:.4f}.hdf5' # hdf의 d와 f는 02d와 4f의 df?
 checkpoint = ModelCheckpoint(filepath = modelpath, monitor = 'acc', save_best_only = True, mode = 'auto')
 
@@ -81,10 +78,7 @@ checkpoint = ModelCheckpoint(filepath = modelpath, monitor = 'acc', save_best_on
 #                       write_graph=True, write_images=True)
 
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['acc'])
-hist = model.fit(x_train, y_train, epochs = 100, batch_size = 1, validation_split = 0.2, callbacks = [es, checkpoint], verbose = 1)
-
-
-
+hist = model.fit(x_train, y_train, epochs = 100, batch_size = 32, validation_split = 0.2, callbacks = [checkpoint], verbose = 1)
 
 # 4. 평가, 예측
 loss, acc  = model.evaluate(x_test, y_test, batch_size = 1)
@@ -95,8 +89,6 @@ print('acc : ', acc)
 y_pred = model.predict(x_test)
 # print(y_pred)
 # print(np.argmax(y_pred, axis = 1))
-
-
 
 # 시각화
 plt.figure(figsize =(10, 6))
@@ -110,8 +102,8 @@ plt.xlabel('epoch')
 plt.legend(loc = 'upper right')
 
 plt.subplot(2, 1, 2)
-plt.plot(hist.hitory['acc'], marker = '*', c = 'green', label = 'acc')
-plt.plot(hist.history['val_acc'], marker = '*', c = 'purple', lbel = 'val_acc')
+plt.plot(hist.history['acc'], marker = '*', c = 'green', label = 'acc')
+plt.plot(hist.history['val_acc'], marker = '*', c = 'purple', label = 'val_acc')
 plt.grid()
 plt.title('acc')
 plt.ylabel('acc')
@@ -121,5 +113,57 @@ plt.show()
 
 '''
 loss :  0.15457082632152203
+acc :  1.0
+'''
+'''
+model.add(Conv2D(11, (2, 2), input_shape = (2, 2, 1)))
+model.add(Conv2D(22, (2, 2), padding = 'same'))
+model.add(Conv2D(44, (2, 2), padding = 'same'))
+model.add(Conv2D(66, (2, 2), padding = 'same'))
+model.add(Conv2D(99, (2, 2), padding = 'same'))
+model.add(Conv2D(111, (2, 2), padding = 'same'))
+model.add(Conv2D(88, (2, 2), padding = 'same'))
+model.add(Conv2D(55, (2, 2), padding = 'same'))
+model.add(Conv2D(33, (2, 2), padding = 'same'))
+model.add(Conv2D(11, (2, 2), padding = 'same'))
+model.add(Flatten())
+model.add(Dense(3, activation = 'softmax'))
+
+epo = 100, batch = 1
+loss :  0.11775056918268093
+acc :  0.9333333373069763
+'''
+
+'''
+model.add(Conv2D(11, (2, 2), input_shape = (2, 2, 1)))
+model.add(Conv2D(22, (2, 2), padding = 'same'))
+model.add(Conv2D(44, (2, 2), padding = 'same'))
+model.add(Conv2D(66, (2, 2), padding = 'same'))
+model.add(Conv2D(99, (2, 2), padding = 'same'))
+model.add(Conv2D(111, (2, 2), padding = 'same'))
+model.add(Conv2D(88, (2, 2), padding = 'same'))
+model.add(Conv2D(55, (2, 2), padding = 'same'))
+model.add(Conv2D(33, (2, 2), padding = 'same'))
+model.add(Conv2D(11, (2, 2), padding = 'same'))
+model.add(Flatten())
+model.add(Dense(3, activation = 'softmax'))
+
+epo = 100, batch 32
+loss :  0.06772632348533184
+acc :  0.9666666388511658
+'''
+'''
+model.add(Conv2D(50, (2, 2), input_shape = (2, 2, 1)))
+model.add(Conv2D(70, (2, 2), padding = 'same'))
+model.add(Flatten())
+model.add(Dense(90))
+model.add(Dense(60))
+model.add(Dense(40))
+model.add(Dense(20))
+model.add(Dense(10))
+model.add(Dense(3, activation = 'softmax'))
+
+epo = 100, batch = 32
+loss :  0.01219688486950948
 acc :  1.0
 '''
