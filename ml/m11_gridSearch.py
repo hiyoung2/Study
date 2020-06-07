8530f750c2b8588b0b982d4c43eabba52d08d607
young2e <64512044+young2e@users.noreply.github.com>
2020-06-08 01:48:42 +0900

    Update m11_gridSearch.py

--- ml/m11_gridSearch.py
+++ ml/m11_gridSearch.py
@@ -6,11 +6,6 @@ from sklearn.svm import SVC
 
 # grid, 격자, 그물 모양 / 그물을 던지면 고기를 싹쓸이해서 잡을 수 있다
 # grid search : 내가 넣어 놓은 모든 조건을 싹쓸이 해서 모델 실행해준다
-# 레이어 구성할 때 dropout로 랜덤하게 노드를 연산에서 일부 빼줬더니 성능이 더 좋았다
-# 드랍아웃과 비슷한 역할을 하는 것이 RandomizedSearchCV이다
-# 그리드 서치 모든 조건 넣는다고 성능 무조건 향상? 놉
-# 그 중에 일부만 사용 : 랜덤 서치
-# 
 
 # 1. 데이터
 iris = pd.read_csv('./data/csv/iris.csv', header = 0)
@@ -30,9 +25,9 @@ parameters = [
 # SVC에서 제공해주는 파라미터들이다, 내용은 서로 모르니까 퉁 치고 넘어가자는 말씀,,,
 # C는 에포?
 # kernerl은 activation 느낌?
-# gemma ? 명확히는 모르겠고, 이런 파라미터들이 있다
+# gamma ? 명확히는 모르겠고, 이런 파라미터들이 있다
 # C = 1 넣고 kernel을 linear, C = 10 넣고 kernel을 linear,,,
-# C = 1 넣고 kernel을 rbf, gemma 0.001, C = 1 넣고, kernel을 rbf, gemma르르 0.001,,,,
+# C = 1 넣고 kernel을 rbf, gemma 0.001, C = 1 넣고, kernel을 rbf, gamma 0.001,,,,
 
 # train에서만 cv가 일어난다?
 # train_test_split을 한 다음에 kfold를 썼으니까
