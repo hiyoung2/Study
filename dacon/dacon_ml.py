7096538fd10094f29c5c20084577a9066735aef9
unknown <pnp3737@gmail.com>
2020-06-08 20:49:19 +0900

    06082049

--- dacon/dacon_ml.py
+++ dacon/dacon_ml.py
@@ -1,9 +1,10 @@
+##### 수정 필요
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
-from keras.models import Model
-from keras.layers import Dense, Dropout, Input
 
 train = pd.read_csv("./data/dacon/comp1/train.csv", header = 0, index_col = 0)
 
@@ -27,23 +28,20 @@ test = test.interpolate()
 train = train.fillna(method = 'bfill')
 print(train.head())
 
-
-# csv 파일 만들기(submit 파일)
-# y_pred.to_csv(경로)
-
-
-
 train = train.values
 test = test.values
 submission = submission.values
 
 print(type(train))
 
+# 1. 데이터
+
+# npy 형식으로 저장
 np.save("./data/dacon/comp1/train.npy", arr = train)
 np.save("./data/dacon/comp1/test.npy", arr = test)
 np.save("./data/dacon/comp1/submission.npy", arr = submission)
 
-
+# 데이터 불러오기
 data = np.load("./data/dacon/comp1/train.npy",  allow_pickle = True)
 x_pred = np.load("./data/dacon/comp1/test.npy", allow_pickle = True)
 y_pred = np.load("./data/dacon/comp1/submission.npy", allow_pickle = True)
@@ -63,50 +61,35 @@ x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size = 0.2, shuffle = True, random_state = 11
 )
 
-# kfold = KFold(n_splits=5, shuffle = True)
 
+# 2. 모델
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestRegressor
 
-def build_model(drop=0.5, optimizer = 'adam') :
-    inputs = Input(shape = (71, ), name = 'inputs')
-    x = Dense(512, activation = 'relu', name = 'hidden1')(inputs)
-    x = Dropout(drop)(x)
-    x = Dense(256, activation = 'relu', name = 'hidden2')(x)
-    x = Dropout(drop)(x)
-    x = Dense(128, activation = 'relu', name = 'hidden3')(x)
-    x = Dropout(drop)(x)
-    outputs = Dense(4, activation = 'softmax', name = 'outputs')(x)
-    model = Model(inputs = inputs, outputs = outputs)
-    model.compile(optimizer = optimizer, metrics = ['mae'],
-                  loss = 'mae')
-    return model
+model = RandomForestRegressor()
 
-def create_hyperparameters() :
-    batchs = [100, 200, 300, 400, 500]
-    optimizers = ['rmsprop', 'adam', 'adadelta']
-    dropout = np.linspace(0.1, 0.5, 5) 
-    return {"batch_size" : batchs, "optimizer" : optimizers, 
-            "drop" : dropout}
+# 3. 훈련
+model.fit(x_train, y_train)
 
-from keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor
+# 4. 평가, 예측
+from sklearn.metrics import mean_absolute_error
 
-model = KerasRegressor(build_fn = build_model, verbose = 1)
+score = model.score(x_test, y_test)
 
-hyperparameters = create_hyperparameters() 
-
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-search = RandomizedSearchCV(model, hyperparameters, cv = 3)
-
-# 3. 훈련(실행)
-search.fit(x_train, y_train)
-
-# 평가, 예측
-
-mae = search.score(x_test, y_test)
+mae = mean_absolute_error(x_test, y_test)
 
 y_pred = model.predict(x_pred)
+
+
+print("mae :", mae)
 print("y_pred :", y_pred)
 
-print("최적의 파라미터 :", search.best_params_)
-print("mae :", mae)
 
 
+# CSV 파일로 최종 변환
+from pandas import DataFrame
+a = np.arange(10000,20000)
+submission = y_pred
+submission = pd.DataFrame(submission,a)
+submission.to_csv("./data/dacon/comp1/sample_submission.csv", header = ["hhb", "hbo2", "ca", "na"], index = True, index_label="id" )
