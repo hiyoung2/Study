2f32366d0966be282fa277572e3407e5233f4f8a
unknown <pnp3737@gmail.com>
2020-07-10 19:49:07 +0900

    commit

--- tf/tf08_mv1.py
+++ tf/tf08_mv1.py
@@ -7,7 +7,7 @@ tf.set_random_seed(777) # 잭 팟
 x1_data = [73., 93., 89., 96., 73.]
 x2_data = [80., 88., 91., 98., 66.]
 x3_data = [75., 93., 90., 100., 70.]
-y_data = [152., 185., 180., 196., 142]
+y_data = [152., 185., 180., 196., 142.]
 
 x1 = tf.placeholder(tf.float32)
 x2 = tf.placeholder(tf.float32)
