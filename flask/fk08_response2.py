502d1b78aeb07057722cd22163d8c210387df5b3
unknown <pnp3737@gmail.com>
2020-07-13 17:59:31 +0900

    0713_rnn

---
+++ flask/fk08_response2.py
@@ -0,0 +1,42 @@
+from flask import Flask, Response, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def response_test() :
+    custom_response = Response('Custom Response', 200, 
+                               {"Program" : "Flask Web Application"})
+    return make_response(custom_response)
+
+# @route의 친구들
+
+@app.before_first_request
+def before_first_request() :
+    print("[1] 앱이 기동되고 나서 첫 번째 HTTP 요청에만 응답합니다.")
+    # print("이 서버는 개인 자산이니 건드리지 말 것")
+    # print("곧 자료를 전송합니다,")
+
+
+@app.before_request 
+def before_reques() :
+    print("[2] 매 HHTP 요청이처리되기 전에 실행됩니다")
+
+@app.after_request
+def after_request(response) :
+    app.run(host = '127.0.0.1')
+    print("[3] 매  HTTP 요청이 처리되고 나서 실행됩니다")
+    return response
+
+@app.teardown_request
+def teardown_request(exception) :
+    print("[4] 매 HTTP 요청의 결과가 브라우저에 응답하고 나서 호출된다.")
+
+@app.teardown_appcontext
+def teardown_appcontext(exception) :
+    print("[5] HTTP 요청의 어플리케이션 컨텍스트가 종료할 때 실행된다")
+
+
+if __name__ == "__main__" :
+    app.run(host = "127.0.0.1") # port 따로 표시 안 함 -> 실행 되고 터미널에 5000으로 뜸
+                                # default : 5000
+
--- tf/tf21_rnn2.py
+++ tf/tf21_rnn2.py
@@ -6,7 +6,7 @@ print(dataset.shape) # (10,)
 
 # RNN 모델을 구성하라
 
-
+size = 6
 def split_x(seq, size):                            
                                                     
     aaa = []                                      
@@ -18,8 +18,16 @@ def split_x(seq, size):
     print(type(aaa))                                
     return np.array(aaa)                            
 
-dataset = split_x(dataset, 6)
+dataset = split_x(dataset, size)
 print(dataset)
+print()
+'''
+[[ 1  2  3  4  5  6]
+ [ 2  3  4  5  6  7]
+ [ 3  4  5  6  7  8]
+ [ 4  5  6  7  8  9]
+ [ 5  6  7  8  9 10]]
+'''
 
 x_data = dataset[:, :-1]
 y_data = dataset[:, -1]
@@ -28,34 +36,55 @@ print(x_data)
 print()
 print(y_data)
 print()
+'''
+x_data
+[[1 2 3 4 5]
+ [2 3 4 5 6]
+ [3 4 5 6 7]
+ [4 5 6 7 8]
+ [5 6 7 8 9]]
 
+y_data
+[ 6  7  8  9 10]
+'''
 print("x_data.shape :", x_data.shape) # (5, 5)
 print("y_data.shape :", y_data.shape) # (5,)
 print()
-x_data = x_data.reshape(1, 5, 5)
-y_data = y_data.reshape(5, 1)
 
-print("x_data.shape :", x_data.shape) # (5, 5, 1)
+
+x_data = x_data.reshape(1, 5, 5)
+y_data = y_data.reshape(1, 5)
+
+print("x_data")
+print(x_data)
+print("y_data")
+print(y_data)
+print()
+
+print("x_data.shape :", x_data.shape) # (1, 5, 5)
 print("y_data.shape :", y_data.shape) # (1, 5)
 print()
 
+
+
 sequence_length = 5
 input_dim = 5
-output = 5
-batch_size = 5 # 전체 행
-lr = 0.1
-
+output = 10 # 10 아래로는 잘못된 인자를 받았다는 InvalidArgumentError 발생함
+batch_size = 1 
+lr = 0.003
 
 X = tf.compat.v1.placeholder(tf.float32, (None, sequence_length, input_dim))
-Y = tf.compat.v1.placeholder(tf.int32, (None, 1))
-print(X) # Tensor("Placeholder:0", shape=(?, 5, 1), dtype=float32)
+Y = tf.compat.v1.placeholder(tf.int32, (None, sequence_length))
+print(X) # Tensor("Placeholder:0", shape=(?, 5, 5), dtype=float32)
 print(Y) # Tensor("Placeholder_1:0", shape=(?, 5), dtype=int32)
 
-cell = tf.keras.layers.LSTMCell(output)
+cell = tf.nn.rnn_cell.BasicLSTMCell(output) 
+# cell = tf.keras.layers.LSTMCell(output)
 
 hypothesis, _states = tf.nn.dynamic_rnn(cell, X, dtype = tf.float32)
 
 print("hypothesis :", hypothesis)
+# hypothesis : Tensor("rnn/transpose_1:0", shape=(?, 5, 5), dtype=float32)
 
 weights = tf.ones([batch_size, sequence_length]) 
 
@@ -67,15 +96,29 @@ cost = tf.reduce_mean(sequence_loss)
 
 train = tf.compat.v1.train.AdamOptimizer(learning_rate = lr).minimize(cost)
 
-# prediction = tf.argmax(hypothesis, axis = 2)
+prediction = tf.argmax(hypothesis, axis = 2)
 
 
 with tf.Session() as sess :
     sess.run(tf.global_variables_initializer())
 
-    for i in range(401) :
+    for i in range(501) :
         loss, _ = sess.run([cost, train], feed_dict = {X:x_data, Y:y_data})
 
-        # result = sess.run(prediction, feed_dict = {X:x_data})
+        result = sess.run(prediction, feed_dict = {X:x_data})
 
-        print(i, "loss :", loss)
+        print(i, "loss :", loss, "\nPredict :", result, "\nTrue_Y :", y_data)
+
+'''
+sequence_length = 5
+input_dim = 5
+output = 11 
+batch_size = 1 
+lr = 0.003
+
+epo = 500
+
+500 loss : 1.2132335
+Predict : [[ 6  7  8  9 10]]
+True_Y : [[ 6  7  8  9 10]]
+'''
